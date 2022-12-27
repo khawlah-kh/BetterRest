d392b4966f4fd74193fe35647be527614b316aa6
Khawlah Khalid <khawlah1920@hotmail.com>
2022-12-27 07:32:55 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Khawlah Khalid on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
